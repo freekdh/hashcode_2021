7086a709aeaacf5728864e969545eaa82e2e0510
freekdh <freekdh@gmail.com>
2021-03-12 16:02:14 -0800

    add setup

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name="trafic_signaling",
+    version="0.1",
+    description="In-process code for trafic signaling project",
+    author="Freek de Haas",
+    packages=["trafic_signaling"],
+)
